110e553d4127b1983af0955a5d67dc816e964484
martinromario55 <martinromario55@gmail.com>
2024-04-17 12:01:28 +0300

    frontend initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-red-500 text-3xl">Hello World</h2>
+    </div>
+  )
+}
+
+export default App
